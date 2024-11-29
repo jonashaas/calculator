25cc75de21e4b4a1716f5ae79bd43b5fb52ec580
JonasHaas <haas.qt@gmail.com>
2024-11-29 13:37:31 +0100

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,10 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  daisyui: {
+    themes: ["light", "dark", "sunset", "emerald"],
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
