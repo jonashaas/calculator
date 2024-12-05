4da53a87d3bafbd394c818c990a7395fad2314d5
JonasHaas <haas.qt@gmail.com>
2024-12-05 14:11:49 +0100

    add themechanger component to navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,30 +1,15 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
+import ThemeChanger from './ThemeChanger';
 export default function NavBar() {
   return (
     <nav className="navbar bg-base-100 shadow-xl">
       <div className="navbar-start pl-4">
-        <Link href="/" className="btn btn-ghost text-xl">Inline Calculator</Link>
+        <Link href="/" className="btn btn-ghost text-2xl"><Image src="/logo.png" alt="Logo" width={32} height={32} /> Inline Calculator</Link>
       </div>
-      {/* <div className="navbar-end pr-4">
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item">10</span>
-          </div>
-        </button>
-      </div> */}
+      <div className="navbar-end pr-4">
+        <ThemeChanger />
+      </div>
     </nav>
   );
 }
