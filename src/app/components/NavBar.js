8f801b5bc8555d8e8247258febf6a40db04ea014
JonasHaas <haas.qt@gmail.com>
2024-12-05 14:33:11 +0100

    remove some padding

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -4,10 +4,10 @@ import ThemeChanger from './ThemeChanger';
 export default function NavBar() {
   return (
     <nav className="navbar bg-base-100 shadow-xl">
-      <div className="navbar-start pl-4">
+      <div className="navbar-start lg:pl-4">
         <Link href="/" className="btn btn-ghost text-2xl"><Image src="/logo.png" alt="Logo" width={32} height={32} /> Inline Calculator</Link>
       </div>
-      <div className="navbar-end pr-4">
+      <div className="navbar-end lg:pr-4">
         <ThemeChanger />
       </div>
     </nav>
