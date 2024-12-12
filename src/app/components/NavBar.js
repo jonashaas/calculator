622a745cb3db33fd1b7b2a5f630cdbd86fc0f5e1
JonasHaas <haas.qt@gmail.com>
2024-12-12 10:20:22 +0100

    change: logoname

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <nav className="navbar bg-base-100 shadow-xl">
       <div className="navbar-start lg:pl-4">
-        <Link href="/" className="btn btn-ghost text-2xl"><Image src="/logo.png" alt="Logo" width={32} height={32} /> itdesign</Link>
+        <Link href="/" className="btn btn-ghost text-2xl"><Image src="/logo.png" alt="Logo" width={32} height={32} />FancyCompany</Link>
       </div>
       <div className="navbar-end lg:pr-4">
         <ThemeChanger />
