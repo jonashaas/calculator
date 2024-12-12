e816d05975368677239a4b745639af89f9460891
JonasHaas <haas.qt@gmail.com>
2024-12-12 10:19:31 +0100

    fix: structure

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Card = ({ children, className = "" }) => {
+export default function Card({ children, className = "" }) {
   return (
     <div className={`card w-full shadow-md bg-base-200 items-start p-4 ${className}`}>
       {children}
     </div>
   );
-};
-
-export default Card;
+}
